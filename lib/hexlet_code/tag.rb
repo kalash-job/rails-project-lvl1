888d84b4e8e6e57c1d27c1cc72c7ccb2d53ce063
Nikolay Kalashnikov <kalash-job@yandex.ru>
2022-05-09 17:42:37 +0300

    add class Tag with method build for unpair tags

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -2,5 +2,15 @@
 
 module HexletCode
   class Tag
+    def build(tag_name, options = {})
+      tag_opening = "<#{tag_name}"
+      tag_closing = ">"
+      return "#{tag_opening}#{tag_closing}" if options.empty?
+
+      result_options = []
+      options.each_pair { |key, value| result_options << "#{key}=\"#{value}\"" }
+      options_line = result_options.join(" ")
+      "#{tag_opening} #{options_line}#{tag_closing}"
+    end
   end
 end

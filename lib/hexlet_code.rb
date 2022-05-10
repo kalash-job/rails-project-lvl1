500865a20b41a14dc1794269b51bca135f349c44
Nikolay Kalashnikov <kalash-job@yandex.ru>
2022-05-10 05:08:53 +0300

    add method form_for

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,9 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.form_for(user, options = {}, &block)
+    url = options.fetch(:url, "#")
+    "<form action=\"#{url}\" method=\"post\">\n</form>"
+  end
 end

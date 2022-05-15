02145d4d1cbf1b9ab45f16b71e3c6a57092349b9
Nikolay Kalashnikov <kalash-job@yandex.ru>
2022-05-15 04:10:52 +0300

    temporary fix for linter requirements

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,7 +4,7 @@ module HexletCode
   class Error < StandardError; end
   autoload(:HexletCode, "./hexlet_code/version.rb")
 
-  def self.form_for(user, options = {}, &block)
+  def self.form_for(_user, options = {}, &_block)
     url = options.fetch(:url, "#")
     "<form action=\"#{url}\" method=\"post\">\n</form>"
   end

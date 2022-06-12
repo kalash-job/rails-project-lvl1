ce7fc4d0de26ac128e7e35584c62289637f24941
Nikolay Kalashnikov <kalash-job@yandex.ru>
2022-06-12 21:24:10 +0300

    fix tests for new version of build()

--- test/lib/hexlet_code/test_tag.rb
+++ test/lib/hexlet_code/test_tag.rb
@@ -3,13 +3,16 @@
 require_relative "./../../test_helper"
 
 class TestTag < Minitest::Test
+  EXPECTED_INPUT_RESULT = { name: "input", options: { type: "submit", value: "Save" }, tag_body: [] }.freeze
+  EXPECTED_LABEL_RESULT = { name: "label", options: { for: "email" }, tag_body: "Email" }.freeze
+
   def test_unpair_tag_creation
-    assert { HexletCode::Tag.build("br") == "<br>" }
-    assert { HexletCode::Tag.build("input", type: "submit", value: "Save") == '<input type="submit" value="Save">' }
+    assert { HexletCode::Tag.build("br") == { name: "br", options: {}, tag_body: [] } }
+    assert { HexletCode::Tag.build("input", type: "submit", value: "Save") == EXPECTED_INPUT_RESULT }
   end
 
   def test_pair_tag_creation
-    assert { HexletCode::Tag.build("div") == "<div></div>" }
-    assert { HexletCode::Tag.build("label", for: "email") { "Email" } == "<label for=\"email\">Email</label>" }
+    assert { HexletCode::Tag.build("div") == { name: "div", options: {}, tag_body: [] } }
+    assert { HexletCode::Tag.build("label", for: "email") { "Email" } == EXPECTED_LABEL_RESULT }
   end
 end
